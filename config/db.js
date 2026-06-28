17fa12f57772fbe0b2847381c461ead06c216909
yossi bar david <bardavid.y@gmail.com>
2026-06-28 20:26:03 +0300

    fix mangos db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
-async function connectDB(){
-    try{
-     await  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gxzgvey.mongodb.net/${process.env.BD_NAME}`);
-     //await  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gxzgvey.mongodb.net/${process.env.BD_NAME}`);
-      console.log("💥 Mongo DB connect successfully 💥");
-        
-    }
-    catch(erorr){
-        console.error("Data DB connction failed 😢");
-        
 
-    }
-    
+async function connectDB() {
+  try {
+    const uri =
+      process.env.MONGO_URI  ;
+    await mongoose.connect(uri);
+    console.log("💥 Mongo DB connected successfully 💥");
+  } catch (error) {
+    console.error("DB connection failed 😞");
+    console.error(error.message);
+  }
 }
-module.exports = connectDB; //הפונקציה הזו זמינה לקבצים אחרים
+
+module.exports = connectDB;
